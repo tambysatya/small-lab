75080140f902703e5c3bc907e8014de210b4641d
Satya TAMBY <tambysatya@gmail.com>
2026-07-26 01:56:00 +0200

    fix problem of merge

--- lib/generators/nixos/vms.nix
+++ lib/generators/nixos/vms.nix
@@ -12,66 +12,67 @@ let
 
   generateConf = infra: vmName: vmConf: 
     {inputs, config, ...}:
-        utils.mergeAll ([{
-
+      {
           imports = let path = inputs.self.outPath;
                     in ["${path}/modules/disko-vm.nix"
                         "${path}/profiles/vm.nix"
                         "${path}/modules/step-renew"
                     ];
+          config = lib.mkMerge ([
+            {
+              boot.loader.systemd-boot.enable = true;
+              boot.loader.efi.canTouchEfiVariables = true;
+              time.timeZone = "Europe/Paris";
+              i18n.defaultLocale = "fr_FR.UTF-8";
 
-          boot.loader.systemd-boot.enable = true;
-          boot.loader.efi.canTouchEfiVariables = true;
-          time.timeZone = "Europe/Paris";
-          i18n.defaultLocale = "fr_FR.UTF-8";
 
 
-
-          networking = {
-            hostName = vmName;
-            interfaces.enp1s0 = {
-              useDHCP = false;
-              ipv4 = {
-                addresses = [
-                  {address = vmConf.ipAddress; prefixLength = 24;}
-                ];
-                routes = [
-                  {address = "0.0.0.0"; via = infra.gateway; prefixLength = 0;}
-                ];
+              networking = {
+                hostName = vmName;
+                interfaces.enp1s0 = {
+                  useDHCP = false;
+                  ipv4 = {
+                    addresses = [
+                      {address = vmConf.ipAddress; prefixLength = 24;}
+                    ];
+                    routes = [
+                      {address = "0.0.0.0"; via = infra.gateway; prefixLength = 0;}
+                    ];
+                  };
+                };
+                nameservers = infra.dns;
               };
-            };
-            nameservers = infra.dns;
-          };
 
-          /* Mounts additional disks */
-          fileSystems = utils.mergeAll (lib.map 
-                          (disk: {
-                                  "${disk.bind}" = {
-                                      device = "/dev/${disk.dst}";   
-                                      fsType = disk.fsType;
-                                      options = disk.options;
-                                  };
-                            }) vmConf.additionalDisks);
-          services.openssh.enable = true;
-          users.users.root.openssh.authorizedKeys.keys = infra.root_ssh_pubkeys;
+              /* Mounts additional disks */
+              fileSystems = utils.mergeAll (lib.map 
+                              (disk: {
+                                      "${disk.bind}" = {
+                                          device = "/dev/${disk.dst}";   
+                                          fsType = disk.fsType;
+                                          options = disk.options;
+                                      };
+                                }) vmConf.additionalDisks);
+              services.openssh.enable = true;
+              users.users.root.openssh.authorizedKeys.keys = infra.root_ssh_pubkeys;
 
-          /* Initializes step-renew */
-          services.step-renew  = {
-              enable = true;
-              caURL = infra.ca.url;
-              caFingerprint = builtins.readFile "${path}/secrets/plain/CA/fingerprint"; # the fingerprint is also generated with the bootstrap script
-          };
+              /* Initializes step-renew */
+              services.step-renew  = {
+                  enable = true;
+                  caURL = infra.ca.url;
+                  caFingerprint = builtins.readFile "${path}/secrets/plain/CA/fingerprint"; # the fingerprint is also generated with the bootstrap script
+              };
 
-          /* Initializes sops */
-          sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+              /* Initializes sops */
+              sops.age.keyFile = "/var/lib/sops-nix/key.txt";
 
-          networking.firewall = { #TODO
-            allowedTCPPorts = [22]; # ++ generateTCPPorts vmConf.services 
-            allowedUDPPorts = []; # ++ generateUDPPorts vmConf.services
-          };
-      }] 
-      ++ (allServicesModules infra vmName vmConf)
-      );
+              networking.firewall = { #TODO
+                allowedTCPPorts = [22]; # ++ generateTCPPorts vmConf.services 
+                allowedUDPPorts = []; # ++ generateUDPPorts vmConf.services
+              };
+          }] ++ 
+          (allServicesModules infra vmName vmConf)
+        );
+      };
 in {
 
   # Generates the configuration of a given VM
