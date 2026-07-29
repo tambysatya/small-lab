a47ebf13790c42b96e4b235e61fce3f9845e4866
Satya TAMBY <tambysatya@gmail.com>
2026-07-30 00:29:18 +0200

    nixos-generator

--- flake.nix
+++ flake.nix
@@ -28,6 +28,56 @@
       pkgs = nixpkgs.legacyPackages.${system};
 
       terranix-generator = (import ./lib/generators/terranix {inherit lib inputs;}).generator;
+      nixos-generator = inventory:
+        let 
+              infra-config = (lib.evalModules 
+                          {modules = [
+                              "${nixpkgs}/nixos/modules/misc/assertions.nix"
+                              ./modules/infra
+                              inventory];}).config;
+              infra = infra-config.infra;
+
+              registry = lib.foldl' 
+                            lib.recursiveUpdate
+                            {}
+                            (lib.mapAttrsToList 
+                                (vmname: vmconf: 
+                                    (lib.evalModules {
+                                            specialArgs = {inherit inputs lib pkgs infra vmname vmconf;};
+                                            modules = [
+                                                ./modules/registry
+                                                ./modules/garage/register.nix
+                                                ./modules/keycloak/register.nix
+                                                ./modules/openldap/register.nix
+                                                ./modules/postgres/register.nix
+                                                ./modules/step-ca/register.nix
+                                                ./modules/nextcloud/register.nix
+                                            ];
+                                        }).config.infra-services.registry)
+                                infra.vms);
+                                    
+               configs = lib.mapAttrs
+                            (vmname: vmconf:
+                                lib.nixosSystem {
+                                    inherit system; 
+                                    specialArgs = {
+                                        inherit inputs infra registry vmname vmconf;
+                                    };
+                                    modules = [./profiles/vm.nix
+                                               ./modules/disko-vm.nix
+                                        ];
+                                })
+                            infra.vms;
+                iso = lib.nixosSystem {
+                        modules = [
+                               "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+                               ./profiles/iso.nix
+                        ];
+                        inherit system;
+                        specialArgs = {inherit inputs;};
+                    };
+
+        in configs // {inherit iso;};
 
       infra-config = (lib.evalModules 
                           {modules = [
@@ -55,34 +105,14 @@
                                 }).config.infra-services.registry)
                         infra.vms);
                             
-       configs = lib.mapAttrs
-                    (vmname: vmconf:
-                        lib.nixosSystem {
-                            inherit system; 
-                            specialArgs = {
-                                inherit inputs infra registry vmname vmconf;
-                            };
-                            modules = [./profiles/vm.nix
-                                       ./modules/disko-vm.nix
-                                ];
-                        })
-                    infra.vms;
+       configs = nixos-generator ./example.nix;
 
     in {
       generators = {
         terranix = terranix-generator; 
+        nixos = nixos-generator;
       };
-      nixosConfigurations = configs // {
-                            iso = lib.nixosSystem {
-                                    modules = [
-                                           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-                                           ./profiles/iso.nix
-                                    ];
-                                    inherit system;
-                                    specialArgs = {inherit inputs;};
-                                };
-                                };
-
+      nixosConfigurations = configs;
       terranixConfigurations =
 
         terranix.lib.terranixConfiguration {inherit system; 
