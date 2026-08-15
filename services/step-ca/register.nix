6b250554811b8337beb0f0fa03bff5512b86ed45
Satya TAMBY <tambysatya@gmail.com>
2026-08-15 21:44:33 +0200

    step files

--- services/step-ca/register.nix
+++ services/step-ca/register.nix
@@ -5,7 +5,7 @@ let
     infralib = import "${inputs.self.outPath}/lib/infra" {inherit lib vmconf vmname;};
     reg = import "${inputs.self.outPath}/lib/registry/register.nix" {inherit inputs lib vmname infra vmconf;};
     secrets = {
-        names = ["ca-password.key"];
+        names = ["ca-password.key"  "intermediate_ca_key"];
         owner = "step-ca";
         kind = {provider = "step";};
     };
