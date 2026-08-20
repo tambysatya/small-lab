5c3f948b4dea31277524bbba3ec40d67301ed1a6
Satya TAMBY <tambysatya@gmail.com>
2026-08-20 15:46:48 +0200

    fix typo

--- lib/compiler/volumes.nix
+++ lib/compiler/volumes.nix
@@ -37,7 +37,7 @@ let
                         hostPath = srcPath;
                         isReadOnly = false;
                     };
-                    systemd.services."containers@${service}.service" = {
+                    systemd.services."containers@${service}" = {
                         after = ["init-volumes.service"];
                         requires = ["init-volumes.service"];
                     };
