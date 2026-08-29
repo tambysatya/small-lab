a3b626cb0a5fc7df81f09616e047a17e58d4a870
satya tamby <tambysatya@gmail.com>
2026-08-29 17:21:24 +0200

    certificates

--- modules/infra/outputs/options/default.nix
+++ modules/infra/outputs/options/default.nix
@@ -2,11 +2,33 @@
 let
     types = lib.types;
 
+    /* We need to manually reimplement some options, otherwise the conflict resolution won't allow
+       us to define infra.outputs.<name>.config from multiple different places */
+
+    config = types.submodule {
+        options ={
+            config.imports = lib.mkOption {
+                description = "List of imports";
+                type = types.listOf (types.either types.str types.path);
+                default = [];
+            };
+            config.sops = lib.mkOption {
+                description = "Sops configuration";
+                type = types.attrs;
+                default = {};
+            };
+            config.services.step-renew = lib.mkOption {
+                description = "Step Renew configuration";
+                type = types.attrs;
+                default = {};
+            };
+        };
+    };
+
     
 in
 {
     options.infra.outputs = lib.mkOption {
         description = "Generated configurations";
-        type = types.attrs;
-    };
-}
+        type = types.attrsOf config;
+    };}
--- modules/infra/outputs/step.nix
+++ modules/infra/outputs/step.nix
@@ -1,12 +1,24 @@
-{lib, inputs, config, pkgs,...}:
+{lib, inputs, config, pkgs,path,...}:
 
 let
     utils = import ./lib {inherit lib inputs;};
+
+    processVM = 
+        vmname: {sslCertificates,...}:{
+            config = {
+                imports = ["${inputs.self.outPath}/services/step-renew"];
+                services.step-renew = {
+                    caURL = "ca.${config.infra.topology.domain}";
+                    caFingerprint = "${path}/.secrets/git/fingerprint";
+                    certs = sslCertificates;
+                };
+            };
+        };
+        
 
 
 
 
 in {
-
-
+    infra.outputs = lib.mapAttrs processVM config.infra.deploy.systems;
 }
