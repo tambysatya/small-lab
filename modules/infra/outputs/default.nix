72b8ef1d220b0dde330743c4a5bf2543f7df9140
Satya Tamby <tambysatya@gmail.com>
2026-09-03 11:13:37 +0200

    users setups

--- modules/infra/outputs/default.nix
+++ modules/infra/outputs/default.nix
@@ -3,41 +3,23 @@
 let
 
     utils = import ../deploy/lib.nix {inherit inputs lib;};
-    processSystem =
-        envuid:
-        deploy@{env, ip, proxy, sops, sslCertificates, storage, users}:
+
+    mkUser = {name, uid}:
         {
-            ${envuid}.config = utils.mergeAll [
-                        (processSops sops)
-                     ];
-        } 
-        //
-        (if env.type == "container"
-            then {
-                ${env.host.vm}.config.sops.secrets."${env.host.container}.key" = {
-                    sopsFile = "${path}/.secrets/enc/${env.host.container}.key";
-                    format = "binary";
-                    restartUnits = ["container@${env.host.container}.service"];
-                    owner = "root";
-                };
-            }
-            else {});
-
-    processSops =
-        sops:
-        let process =
-                {filename, owner, reload, mode}:
-                {
-                    sops.secrets."${filename}" = {
-                        sopsFile = "${path}/.secrets/enc/${filename}.enc";
-                        format = "binary";
-                        restartUnits = reload;
-                        inherit owner mode;
-                    };
-                };
-            initSops = {sops.age.keyFile = "/var/lib/sops-nix/key.txt";};
-        in utils.mergeAll ([initSops] ++ map process sops);
-
+            users.${name} = {
+               inherit uid;
+               group = name;
+               isSystemUser = true;
+            };
+            groups.${name} = {
+                gid = uid;
+            };
+        };
+    processUsers = 
+        name: deploy:
+        {
+            config.users = utils.mergeAll (map mkUser deploy.users);
+        };
 in
 
 {
@@ -47,4 +29,5 @@ in
                ./network.nix
               ];
     #infra.outputs = utils.mergeAll (lib.mapAttrsToList processSystem config.infra.deploy.systems);
+    infra.outputs = lib.mapAttrs processUsers config.infra.deploy.systems;
 }
