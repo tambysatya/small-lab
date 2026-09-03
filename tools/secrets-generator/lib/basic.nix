802457103e1f2d5193886c0634a0173a2e7b7b42
Satya Tamby <tambysatya@gmail.com>
2026-09-03 16:08:10 +0200

    changed the structure of .secrets

--- tools/secrets-generator/lib/basic.nix
+++ tools/secrets-generator/lib/basic.nix
@@ -4,29 +4,33 @@ let
     generateIdentity = 
         env:
         if env.type == "vm" then ''
-            mkdir -p ".secrets/provisioner/${utils.envUID env}"
+            mkdir -p ".secrets/perVM/${utils.envUID env}"
         ''
         else ''
-            mkdir -p ".secrets/provisioner/${env.host.vm}/${env.host.container}"
+            mkdir -p ".secrets/perVM/${env.host.vm}/${env.host.container}"
         '';
     give = filename: env: 
         let filepath = ".secrets/plain/${filename}";
             target = if env.type == "vm"
-                     then ".secrets/provisioner/${env.host}/${filename}"
-                     else ".secrets/provisioner/${env.host.vm}/${env.host.container}/${filename}";
+                     then ".secrets/perVM/${env.host}/${filename}"
+                     else ".secrets/perVM/${env.host.vm}/${env.host.container}/${filename}";
         in ''cp ${filepath} ${target}'';
 
     ship = 
         name:
         let 
-            path = ".secrets/provisioner";
+            path = ".secrets/perVM";
+            provisioner = ".secrets/provisioner";
+            tokens = ".secrets/tokens";
         in ''
+            mkdir -p ${provisioner}
+            mkdir -p ${tokens}
             tar -cvf ${path}/${name}.tar ${path}/*
             ${lib.getExe pkgs.gzip} ${path}/${name}.tar
 
             TOKEN=$(${lib.getExe pkgs.openssl} rand -hex 64)
-            mv ${path}/${name}.tar.gz "${path}/$TOKEN.tar.gz"
-            echo "$TOKEN" > ".secrets/${name}.token"
+            mv ${path}/${name}.tar.gz "${provisioner}/$TOKEN.tar.gz"
+            echo "$TOKEN" > "${tokens}/${name}.token"
         '';
         
 
